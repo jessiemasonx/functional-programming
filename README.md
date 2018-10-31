ece8f6ca715b227115a7e19533d5032824732c14
Jessie Mason <jessiemasonx@hotmail.com>
2018-10-31 16:21:43 +0100

    changed Dennis' code

--- README.md
+++ README.md
@@ -11,9 +11,11 @@ moet eerst nog Xcode installeren maar duurt jaren
 
 ### Day 2
 At school, geen OBA  
+onderzoeks vragen halen uit de dataS
+xcode geinstalleeeeerd
 
 
-5 dingen uit data halen die je zou kunnen visuaiseren
+#### 5 dingen uit data halen die je zou kunnen visuaiseren
 
 eindelijk gefixt dat k geen errors meer heb
 
@@ -23,3 +25,33 @@ eindelijk gefixt dat k geen errors meer heb
 4. __By how much has the amount of released school books changed over the past years?__
 5. By how much has the amount of e-books changed over the last years.
 6. __Is de verhouding tussen mannen en vrouwen die romans schrijven veranderd in de afgelopen jaren?__
+
+
+#### Variables for number 4:
+- boek soort (schoolboek of niet)
+- jaar van uitgave
+
+#### Deelvragen for number 4:
+- Hoe veel schoolboeken zijn er in 2017 uitgegeven?
+- In welk jaar zijn de meeste schoolboeken uitgebracht?
+- In welk jaar zijn de minste schoolboeken uitgebracht?
+
+#### What variables would I need for number 6:
+- boek genre (roman)
+- geslacht schrijver
+- jaar uitgave
+
+#### Deelvragen for number 4:
+- Hoe veel mannen hebben in 2017 een roman uitgebracht?
+- Hoe veel vrouwen hebben in 2017 een roman uitgebracht?
+- In welk jaar hebben de meeste vrouwen een roman uitgebracht?
+- In welk jaar hebben de meeste mannen een roman uitgebracht?
+- Hoe veel mannelijke schrijven schrijven romans?
+- Hoe veel vrouwelijke schrijven schrijven romans?
+
+
+### Day 3
+
+- met linda gehad over de onderzoeksvragen
+- deelvragen bedacht
+- code van dennis gekregen en gebruikt
--- index.js
+++ index.js
@@ -1,31 +1,43 @@
-const OBA = require('oba-api');
+require('dotenv').config()
 
-// const dotenv = require('dotenv').config()
+const api = require('./oba-api.js')
+const chalk = require('chalk');
+const express = require('express')
+const app = express()
+const port = 3000
 
-if(process.env.NODE.ENV !== "production") {
-	require('dotenv').load()
-}
-
-
-// Setup authentication to api server
-const client = new OBA({
-  // ProQuest API Keys
-  public: '1e19898c87464e239192c8bfe422f280',
-  secret: '4289fec4e962a33118340c888699438d'
-});
-
-// General usage:
-// client.get({ENDPOINT}, {PARAMS});
-// ENDPOINT = search | details | refine | schema | availability | holdings
-// PARAMS = API url parameter options (see api docs for more info)
-
-// Client returns a promise which resolves the APIs output in JSON
-
-// Example search to the word 'rijk' sorted by title:
-client.get('search', {
-  q: 'Music',
-  sort: 'title'
+const obaApi = new api({
+  url: 'https://zoeken.oba.nl/api/v1/',
+  key: process.env.PUBLIC
 })
-  	// .then(res => console.log(res)) // JSON results
-	.then(res => console.log(JSON.parse(res))) // JSON results
-  	.catch(err => console.log(err)) // Something went wrong in the request to the API
+
+// Search for method, params and than optional where you wanna find something
+// returns first 20 items
+// obaApi.get(endpoint, params, filterKey)
+// possible endpoints: search (needs 'q' parameter) | details (needs a 'frabl' parameter) | availability (needs a 'frabl' parameter) | holdings/root | index/x (where x = facet type (like 'book' ))
+// possible parameters: q, librarian, refine, sort etc. check oba api documentation for all
+// possible filterKey: any higher order key in response object, like title returns only title objects instead of full data object
+obaApi.get('search', {
+  q: 'jessie',
+  librarian: true,
+  refine: true,
+  facet: 'type(book)'
+}, 'title').then(response => {
+
+  // response ends up here
+  console.log(response)
+  // let keys = Object.keys(data[0])
+  // console.log(facet)
+
+  // Make server with the response on the port
+  app.get('/', (req, res) => res.json(response))
+  app.listen(port, () => console.log(chalk.green(`Listening on port ${port}`)))
+})
+
+//Get an idea of what's in the data
+function getKeys(data){
+	console.table(data)
+	let keys = Object.keys(data[0])
+	console.log("Keys: ", keys)
+	return keys
+}
