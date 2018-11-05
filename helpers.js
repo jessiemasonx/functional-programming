e528b08b90a557d89dfac45d0f7646a70b301c1a
Jessie Mason <jessiemasonx@hotmail.com>
2018-11-05 15:49:48 +0100

    can now show more than 20 results

---
+++ helpers.js
@@ -0,0 +1,61 @@
+// Shout out naar Maikel en Chelsea voor het helpen
+// const getResultsFromSearchData = (searchData) => {
+//     return searchData.aquabrowser
+//         && searchData.aquabrowser.results
+//         && searchData.aquabrowser.results[0]
+//         && searchData.aquabrowser.results[0].result
+//         || []
+// }
+
+const getAuthorFromResult = (result) => {
+    return result.authors
+        && result.authors["main-author"]
+        && result.authors["main-author"].$t
+        || undefined
+}
+
+const getTitleFromResult = (result) => {
+    return result.titles
+        && result.titles.title
+        && result.titles.title.$t
+        || undefined
+}
+
+const getPublisherFromResult = (result) => {
+    return result.publication
+        && result.publication.publishers
+        && result.publication.publishers.publisher
+        && result.publication.publishers.publisher.$t
+        || undefined
+}
+
+const getPublicationYearFromResult = (result) => {
+    return result.publication
+        && result.publication.year
+        && result.publication.year.$t
+		&& Number(result.publication.year.$t)
+        || undefined
+}
+
+const getLanguageFromResult = (result) => {
+    return result.languages
+        && result.languages.language
+        && result.languages.language.$t
+        || undefined
+}
+
+
+// help to Maikel and Chelsea
+const getTransformedResultFromResults = (results) => {
+    return results
+        ? results.map(result => ({
+            author: getAuthorFromResult(result),
+            title: getTitleFromResult(result),
+            publicationYear: getPublicationYearFromResult(result),
+            language: getLanguageFromResult(result)
+        }))
+        : []
+}
+
+
+module.exports = {getTransformedResultFromResults}
--- index.js
+++ index.js
@@ -1,33 +1,28 @@
 require('dotenv').config()
 
-const api = require('./oba-api.js')
+// const api = require('./oba-api.js')
 const chalk = require('chalk');
 const express = require('express')
 const app = express()
 const port = 3000
 // Chelsea & Maikel
 const helpers = require ("./helpers.js")
+const obawrapper = require ("./obawrapper.js")
 
 
-const obaApi = new api({
-    url: 'https://zoeken.oba.nl/api/v1/',
-    key: process.env.PUBLIC
+const obaApi = new obawrapper({
+	public: process.env.PUBLIC,
+	secret: process.env.SECRET
 })
 
-// Search for method, params and than optional where you wanna find something
-// returns first 20 items
-// obaApi.get(endpoint, params, filterKey)
-// possible endpoints: search (needs 'q' parameter) | details (needs a 'frabl' parameter) | availability (needs a 'frabl' parameter) | holdings/root | index/x (where x = facet type (like 'book' ))
-// possible parameters: q, librarian, refine, sort etc. check oba api documentation for all
-// possible filterKey: any higher order key in response object, like title returns only title objects instead of full data object
-
-const search = async (q, facet, page) => {
+const search = async (q, facet, page, count) => {
   	return await obaApi.get("search", {
   	  	q,
 		librarian: true,
 		refine: true,
 		facet,
-		page
+		page,
+		count: 20
   	})
 }
 
@@ -35,50 +30,22 @@ const search = async (q, facet, page) => {
 (async () => {
 	try {
 		// Chelsea & Maikel
-		const { data: searchData } = await search("language:dut", ["type(book)", "genre(romantisch-verhaal)", "pubYear(2005)"], 1)
+		const results = await search("language:dut", ["type(book)", "genre(romantisch-verhaal)", "pubYear(2018)"], 1)
 
-		if (searchData) {
-			const results = helpers.getResultsFromSearchData(searchData)
+		if (results) {
+			// const results = helpers.getResultsFromSearchData(searchData)
 			const transformedResults = helpers.getTransformedResultFromResults(results)
 			console.log(transformedResults)
-			app.get("/", (req, res) => res.json(searchData))
+
+			const dataWrapper = {
+				"results": transformedResults
+			}
+			app.get("/", (req, res) => res.json(dataWrapper))
 			app.listen(port, () => console.log(chalk.green(`Listening on port ${port}`)))
 		}
 	} catch (error) {
 		throw new Error(error)
 	}
 })()
-
-
-// OLD CODE
-// obaApi.get('search', {
-//         q: 'language:dut',
-//         librarian: true,
-//         refine: true,
-//         // page: 1,
-//         facet: ['type(book)', 'genre(romantisch-verhaal)', 'pubYear(2000)']
-//     },'author')
-//     .then(response => {
-//         // response ends up here
-//         console.log(response)
-//         // Make server with the response on the port
-// 		app.listen(port, () => console.log(chalk.green(`Listening on port ${port}`)))
-//         app.get('/', (req, res) => res.json(response))
-//     })
-
-
-//
-// obaApi.get('search', {
-//   q: 'language:dut',
-//   librarian: true,
-//   refine: true,
-//   // count: 1,
-//   facet: ['type(book)', 'genre(romantisch-verhaal)', 'pubYear(2000)']
-// }, 'author')
-// .then(response => {
-//   // response ends up here
-//   console.log(response)
-//   // Make server with the response on the port
-//   // app.get('/', (req, res) => res.json(response))
-//   // app.listen(port, () => console.log(chalk.green(`Listening on port ${port}`)))
-// })
+	   //
+       // refine: true,
